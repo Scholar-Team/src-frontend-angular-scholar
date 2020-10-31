8c19f082d256b0f6a0a735a3e47e7fb888224969
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2020-10-30 22:20:58 -0300

    :sparkles: Add Environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  url_api: 'https://project-scholar.herokuapp.com/',
+  authorization: 'c2Nob2xhci1mcm9udDpMenF3WVpJYmVvQlVoZnNLZHZ6S1ZVN3JzdFdieklZOTVjdlUxMVY1',
+  logRequest: true,
+  traceRequest: true
+};
