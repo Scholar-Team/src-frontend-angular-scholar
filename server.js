454771b9a28e2174edc9e1793cdf020b6f31ad4a
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2020-11-13 19:29:28 -0300

    :rocket: deploy

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+const nomeApp = capitalizeFirstLetter(process.env.npm_package_name);
 const app = express();
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
