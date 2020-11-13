073810a924e30348157477d8d72b5ffcc032152a
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2020-11-13 19:57:27 -0300

    test

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const path = require('path');
-const nomeApp = process.env.npm_package_name;
 const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
+const server = require('http').Server(app);
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(__dirname, 'dist', {index: false}));
 
-app.get('/*', (req, res) => {
+/*app.get('/*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});*/
+
+app.get('', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
